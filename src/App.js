6d530a2c9da3e5ce9c8eadf8b3c9a233279bf14e
Joel Luis <joelluis73@gmail.com>
2020-11-25 21:49:34 -0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { Icon } from 'react-icons-kit';
+import { linkedinSquare } from 'react-icons-kit/fa/linkedinSquare';
+import { github } from 'react-icons-kit/fa/github';
+import foto from './foto.jpg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div id="container">
+        <div id="header" className="box-shadow">
+          <span>Joel Luís</span>
+          <span>joel-luis@hotmail.com</span>
+        </div>
+
+        <div id="main">
+          <div className="box box-shadow">
+            <img src={foto} />
+            <span className="tit">Joel Luís Silva</span>
+            <span className="subtit">Frontend Developer</span>
+
+            <div className="contacts">
+              <a
+                href="https://github.com/joel-luis"
+                style={{ color: '#6547a0' }}
+              >
+                <div style={{ width: 70, height: 70 }}>
+                  <Icon size={'100%'} icon={github} />
+                </div>
+              </a>
+              <a
+                href="https://www.linkedin.com/in/joel-luiss/"
+                style={{ color: '#326d98' }}
+              >
+                <div style={{ width: 70, height: 70 }}>
+                  <Icon size={'100%'} icon={linkedinSquare} />
+                </div>
+              </a>
+            </div>
+            <div className="summary">
+              <p>
+              Entusiamado com tecnologia desde pequeno, sou um desenvolvedor 
+              muito focado em aplicações Frontend.
+              </p>
+              <p>
+                D Tenho 5 anos de experiência, atualmente venho me aprimorando na 
+                stack Javascript, ReactJS, Git, MySQL, HTML 5 e CSS 3. 
+                Além de projetos reais estou sempre trabalhando em projetos 
+                como estudo para aprimorar meus conhecimentos e contribuir com 
+                soluções modernas.
+              </p>
+            </div>
+
+            <br />
+            <span className="txt-skills">MINHAS HABILIDADES</span>
+            <div className="skills">
+              <span>ReactJS</span>
+              <span>Javascript</span>
+              <span>NodeJS</span>
+              <span>TypeScript</span>
+              <span>MysSQL</span>
+              <span>HTML 5</span>
+              <span>CSS 3</span>
+              <span>Docker</span>
+              <span>Git / GitFlow</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
